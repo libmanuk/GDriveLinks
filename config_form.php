9d90ddbb4e3add99717aefb34a1d7163ac6299c8
Eric C. Weig <eweig@email.uky.edu>
2020-02-20 07:40:37 -0500

    Update config_form.php

--- config_form.php
+++ config_form.php
@@ -5,16 +5,14 @@ $embed_width_value = get_option('default_embed_width_value');
 $embed_height_value = get_option('default_embed_height_value');
 $view = get_view();
 ?>
+
 <div class="field">
-	<h3>Default Target Value for GoogleDoc Link</h3>
+	<h3>GoogleDoc Link Behavior</h3>
     <div class="inputs">
         <div class="input-block">
-            <div class="input">
-                <?php echo $view->formTextarea('link_target_value', $link_target_value, array('rows' => '3', 'cols' => '50', 'class' => array('textinput'))); ?>
+            <div class="inputs five columns omega">
+  <?php echo get_view()->formCheckbox('link_target_value', null, array('checked' => $link_target_value)); echo " Checking this box sets the hyperlink to a GoogleDrive item to open in a new tab or window."; ?>
             </div>
-			<p class="explanation">
-				Enter text '_blank' or '_self'.  The value '_blank' sets the hyperlink to a GoogleDrive item to open in a new tab or window.  The value '_self' sets the hyperlink to a GoogleDrive item to open in the current frame.  The default value is '_blank'.
-			</p>
         </div>
     </div>
 </div>
